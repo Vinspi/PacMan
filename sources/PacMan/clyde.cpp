d733b54c6c4481882ed8f57e70ee762f70bc9dd1
Vinspi <vinspi13@gmail.com>
2017-11-13 16:55:48 +0100

    IA clyde en cours

---
+++ sources/PacMan/clyde.cpp
@@ -0,0 +1,6 @@
+#include "clyde.h"
+
+Clyde::Clyde() : Entity("../PacMan/graphics_pacman/Clyde.png")
+{
+    setVitesse(1);
+}
---
+++ sources/PacMan/clyde.h
@@ -0,0 +1,11 @@
+#ifndef CLYDE_H
+#define CLYDE_H
+#include "entity.h"
+
+class Clyde : public Entity
+{
+public:
+    Clyde();
+};
+
+#endif // CLYDE_H
